c76e429b2926bb3b99218bc2111de47a9c06d8d2
jiranzo <jiranzo@iti.es>
2024-10-21 00:02:10 +0200

    Inicio proyecto

---
+++ bukowski-project-frontend/src/app/control-panel-page/control-panel-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-panel-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './control-panel-page.component.html',
+  styleUrl: './control-panel-page.component.css'
+})
+export class ControlPanelPageComponent {
+
+}
